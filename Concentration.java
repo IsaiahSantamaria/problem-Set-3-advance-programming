c3fab44d0ea42cfd307edb481bc2dc4741208e8f
IsaiahSantamaria <isaiahasant@gmail.com>
2026-03-21 16:46:20 -0400

    replace the box class with matrix since it makes more senese, now it is almost time to start the gui

--- Concentration.java
+++ Concentration.java
@@ -6,20 +6,26 @@
  */
 public class Concentration {
     public static void main(String []args){
-        Box matrix = new Box();
-        System.out.print(matrix);
+        Matrix matrix = new Matrix();
+        System.out.println(matrix + "\n");
+        matrix.start();
+        System.out.println(matrix);
 
     }
     
 }
 
-class Box {
-    protected Integer [][] intMatrix;
-    protected boolean [][] booleanMatrix;
+class Matrix {
+    private Integer [][] intMatrix;
+    private boolean [][] booleanMatrix;
+    private boolean [][] completedMatrix;
+    private Integer [] values;
 
-    public Box(){
+    public Matrix(){
         intMatrix = new Integer[6][6];
         booleanMatrix = new boolean[6][6];
+        completedMatrix = new boolean[6][6];
+        values = new Integer[18];
     }
     /**
      * this method fills the row and col of the intMatrix 
@@ -34,7 +40,6 @@ class Box {
         booleanMatrix[row][col] = true;
     
     }
-
     /**
      * returns if booleanMatrix[row][col] is empty or not
      * @param row
@@ -44,6 +49,17 @@ class Box {
     public boolean isEmpty(int row, int col){
         return !booleanMatrix[row][col];
     }
+    
+    /**
+     * returns value of the matrix row and column
+     * @param row
+     * @param col
+     * @return value of the matrix row and column
+     */
+    public Integer getVal(int row, int col){
+
+        return intMatrix[row][col];
+    }
 
     /**
      * method restarts both intMatrix and booleanMatrix
@@ -51,8 +67,101 @@ class Box {
     public void restart(){
         booleanMatrix = new boolean[6][6];
         intMatrix = new Integer[6][6];
+        values = new Integer[18];
+    }
+    /**
+     * method starts the game by assigning all values to t
+     * intMatrix
+     */
+    public void start(){
+        int ranRowOne = (int)(Math.random() * 6); //random row coordinate
+        int ranColOne = (int)(Math.random() * 6); //random col coordinate
+        int ranRowTwo = (int)(Math.random() * 6); //random row coordinate
+        int ranColTwo = (int)(Math.random() * 6); //random col coordinate
+        setValues();
+        //randomly assigning 2 coordinates of int matrix
+        for(int i = 0; i < values.length; i ++){
+            //ensure that no matrix is overwritten
+            //does this loop into it finds a matrix where it can put a new val
+            while((ranRowOne == ranRowTwo && ranColOne == ranColTwo) || !isEmpty(ranRowOne,ranColOne) || !isEmpty(ranRowTwo, ranColTwo)){
+                if(ranRowOne == ranRowTwo && ranColOne == ranColTwo){
+                    //System.out.println("loop 1 is repeated");
+                    ranRowOne = (int)(Math.random() * 6); //random row coordinate
+                    ranColOne = (int)(Math.random() * 6); //random col coordinate
+                    ranRowTwo = (int)(Math.random() * 6); //random row coordinate
+                    ranColTwo = (int)(Math.random() * 6); //random col coordinate      
+                }else if(!isEmpty(ranRowOne,ranColOne)){
+                  //  System.out.println("loop 2 is repeated");
+                    ranRowOne = (int)(Math.random() * 6); //random row coordinate
+                    ranColOne = (int)(Math.random() * 6); //random col coordinate
+                }else if(!isEmpty(ranRowTwo, ranColTwo)){
+                   ///System.out.println("loop 3 is repeated");
+                    ranRowTwo = (int)(Math.random() * 6); //random row coordinate
+                    ranColTwo = (int)(Math.random() * 6); //random col coordinate  
+                }
+                
+            }
+            //filling booleanMatrix coordinate to true and 
+            //setting up vals in intMatrix coordinates
+            booleanMatrix[ranRowOne][ranColOne] = true;
+            booleanMatrix[ranRowTwo][ranColTwo] = true;
+            intMatrix[ranRowOne][ranColOne] = values[i];
+            intMatrix[ranRowTwo][ranColTwo] = values[i];
+        }
     }
 
+    /**
+     * setting values for the values array to be imported when
+     * game starts
+     */
+    private void setValues(){
+        int val = (int)(Math.random() * 99);
+        for(int i = 0; i < values.length; i ++){
+            while(isValueRepeated(val)){
+                val = (int)(Math.random() * 99);
+            }
+            values[i] = val;
+        }
+
+    }
+
+    /**
+     * returns if values vals are repeated  
+     * @param val
+     * @return if values vals are repeated 
+     */
+    private boolean isValueRepeated(int val){
+        for(int i = 0; i < values.length; i ++){
+            if(values[i] != null && values[i] == val )return true;
+        }
+        return false;
+    }
+
+    /**
+     * when gui is made, this method will check
+     * if both values coordinate is pressed
+     * @param rowOne
+     * @param colOne
+     * @param rowTwo
+     * @param colTwo
+     */
+    public void bothPressed(int rowOne, int colOne, int rowTwo, int colTwo){
+        if(intMatrix[rowOne][colOne] == intMatrix[rowTwo][colTwo]){
+            System.out.println("There is a Match!");
+            intMatrix[rowOne][colOne] = null;
+            intMatrix[rowTwo][colTwo] = null;
+            completedMatrix[rowOne][colOne] = true;
+            completedMatrix[rowTwo][colTwo] = true;
+        }else{
+            System.out.println("These are not a match");
+        }
+        
+    }
+
+
+    /**
+     * @return output of the Box class Matrixs data
+     */
     @Override
     public String toString(){
         String output ="";
